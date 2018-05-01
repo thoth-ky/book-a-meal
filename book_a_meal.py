a91dea71b50a6eaecafda9f83d1b2ab6bb6e24b9
Joseph M Kyalo <jmutuku95@gmail.com>
2018-05-01 10:35:15 +0300

    Initial DB settings

--- book_a_meal.py
+++ book_a_meal.py
@@ -4,14 +4,22 @@ This script creates a flask app and runs it at specified host and port,
 import os
 # local imports
 try:
-    from app import create_app
+    from app import create_app, DB
 except ModuleNotFoundError:
-    from .app import create_app
+    from .app import create_app, DB
 
 # get configuration environment
 CONFIG = os.environ.get('APP_SETTINGS') or 'development'
 
 APP = create_app(CONFIG)
+
+@APP.shell_context_processor
+def  make_shell_context():
+    '''
+    context when flask shell runs to avoid cumbersome imports'''
+    return {
+        'db': DB
+    }
 
 
 if __name__ == '__main__':

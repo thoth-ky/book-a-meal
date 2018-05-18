d767333c748caa69718671e5c5e3be5fdaeb3634
Joseph M Kyalo <jmutuku95@gmail.com>
2018-05-19 00:05:22 +0300

    [chore] update tests

--- app/helpers/email.py
+++ app/helpers/email.py
@@ -7,25 +7,23 @@ from flask_mail import Message
 # local imports
 from .. import MAIL
 from ..models.models import User
-# app_context = current_app.app_context()
-# app_context.push()
 
-def send_async_email(msg):
+
+def send_async_email(app, msg):  # pragma: no cover
     '''send asynchronous emails'''
-    with current_app.app_context():
-        with MAIL.connect() as conn:
-            conn.send(msg)
+    with app.app_context():
+        MAIL.send(msg)
 
 
 def send_email(subject, sender, recipients, html_body):
     '''create email and send it using a thread'''
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.html = html_body
-    MAIL.send(msg)
-    # Thread(target=send_async_email, args=(msg,)).start()
+    thread = Thread(target=send_async_email, args=[current_app, msg])
+    thread.start()
 
 
-def send_updated_menu(menu):
+def send_updated_menu(menu): # pragma: no cover
     '''Send updated menu, to all clients'''
     # introduce option to opt out of notifications in User model
     users = User.get_all()

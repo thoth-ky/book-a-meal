fa69605e7698b4c74eca7eabb53095344d345c41
Kyalo M <jmutuku95@gmail.com>
2018-06-20 10:39:03 +0300

    [chore] Reduce arguments in function send_email

--- app/helpers/email.py
+++ app/helpers/email.py
@@ -8,25 +8,25 @@ from .. import MAIL
 from ..models.authmodels import User
 
 
-def send_email(subject, sender, recipients, html_body, app):  # pragma: no cover
+def send_email(message, app):  # pragma: no cover
     '''create email and send it using a thread'''
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.html = html_body
     with app.app_context():
-        MAIL.send(msg)
+        MAIL.send(message)
 
 def send_updated_menu(menu):  # pragma: no cover
     '''Send updated menu, to all clients'''
     # introduce option to opt out of notifications in User model
     users = User.get_all()
     subject = "Today's Menu Updates"
-    sender = current_app.config.get('ADMINS')[0]
-    
+    sender = current_app.config.get('ADMINS')[0]  
     for user in users:
         recipients = [user.email]
         html_body = render_template('email/menu.html', user=user, menu=menu)  # pragma: no cover
+        # create message
+        msg = Message(subject, sender=sender, recipients=recipients)
+        msg.html = html_body
 
         t = Thread(target=send_email,
-                   args=[subject, sender, recipients, html_body],
+                   args=[msg],
                    kwargs={'app': current_app._get_current_object()})
         t.start()

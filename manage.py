0988ce131ce49edb6e792c60525863c2171e1482
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-09-04 13:03:47 +0300

    [CH] Add command to create default meals

--- manage.py
+++ manage.py
@@ -6,10 +6,10 @@ from flask_migrate import Migrate, MigrateCommand
 # local imports
 try:
     from app import create_app, DB
-    from app.models import User
+    from app.models import User, Meal
 except ModuleNotFoundError:
     from .app import create_app, DB
-    from .app.models import User
+    from .app.models import User, Meal
 
 # initialize app with all its configs
 APP = create_app(config_name=os.getenv('APP_SETTINGS'))
@@ -50,6 +50,35 @@ def make_admin():
             print('User {} successfully promoted to admin'.format(username))
     else:
         print("That user does not exist")
+
+@MANAGER.command
+def create_default_meals():
+    '''function to make users admin'''
+    name = input('Enter name of the meal: ')
+    while True:
+        price = input('Enter price: ')
+        try:
+            price = int(price)
+            break
+        except:
+            print('Price should be integer')
+            continue
+    
+    description = input('Add a short description of the meal: ')
+    while True:
+        username = input('Enter name of caterer in charge of this meal: ')
+        user = User.get(username=username)
+        if isinstance(user, User):
+            break
+        else:
+            print('User not available, please provide a valid username.')
+            continue
+
+
+    meal = Meal(name=name, price=price, description=description, default=True)
+    meal.user_id = user.user_id
+    meal.save()
+    print('Meal successfully created as default.')
 
 
 
